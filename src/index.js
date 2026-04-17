112c19ad71ed57448b605ae4fc51eaee333a5e56
kevin930321 <78043580+kevin930321@users.noreply.github.com>
2026-04-17 23:33:00 +0800

    Revert "Refactor project into src-based architecture"

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-try {
-  const bot = require("./bot/TradingBotApp");
-  module.exports = bot;
-} catch (error) {
-  console.error("Fatal application bootstrap error:", error);
-  process.exit(1);
-}
